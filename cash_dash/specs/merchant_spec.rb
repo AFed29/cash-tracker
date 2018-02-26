8473bac57c63fba74701bce923ee3ec16f4e7319
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-02-26 00:06:24 +0000

    Added method to save merchant

---
+++ cash_dash/specs/merchant_spec.rb
@@ -0,0 +1,20 @@
+require('minitest/autorun')
+require_relative('../models/merchant.rb')
+
+
+class TestMerchant < MiniTest::Test
+
+  def setup()
+    @merchant1 = Merchant.new({'name' => 'Tesco'})
+  end
+
+  def test_check_merchant_has_name()
+    assert_equal("Tesco", @merchant1.name())
+  end
+
+  def test_save_returns_id()
+    @merchant.save()
+    assert(@merchant1.id != nil)
+  end
+
+end
