ab0b113467c71dd4878ace3f18b1415c5f9face3
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-02-27 14:46:04 +0000

    Basic CSS added

--- cash_dash/models/transaction.rb
+++ cash_dash/models/transaction.rb
@@ -84,7 +84,7 @@ class Transaction
     return Transaction.new(transaction_hash)
   end
 
-  def self.display_pounds_pence(amount)
+  def self.display_pounds_pence( amount )
     pounds = amount/100
     pence = amount - (pounds*100)
     pence = pence.to_s.rjust(2, "0")
